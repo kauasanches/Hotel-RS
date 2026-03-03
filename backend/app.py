fd784b434cc3f4fa78bed5d99828bed7f0bf271c
Kauã Sanches <119595660+kauasanches@users.noreply.github.com>
2026-03-03 08:39:57 -0300

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+# Caminho base do projeto (uma pastacima do backend)
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Pasta frontend (HTML, JS)
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
+
+# Pasta static (CSS)
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/" + STATIC_DIR)
 
 @app.route("/")
-
 def home():
     return "Bom dia galera 2B!"
 
