343bf851af3849b6a94341514afe0a5c246e2132
Kauã Sanches <119595660+kauasanches@users.noreply.github.com>
2026-03-05 12:08:33 -0300

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -10,10 +10,27 @@ FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
 # Pasta static (CSS)
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
-app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/" + STATIC_DIR)
+app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/static")
 
+# Pagina principal
 @app.route("/")
 def home():
-    return "Bom dia galera 2B!"
+    print(send_from_directory(FRONTEND_DIR, "consultar.html"))
+    # return "Bom dia galera 2B!"
+    return send_from_directory(FRONTEND_DIR, "index.html")
 
-app.run()
+# Pagina de consulta
+@app.route("/consulta")
+def consulta_page():
+    return send_from_directory(FRONTEND_DIR, "consultar.html")
+
+# Pagina de alteracao
+@app.route("/alterar")
+def alterar_page():
+    return send_from_directory(FRONTEND_DIR, "alterar.html")
+
+if __name__ == "__main__":
+    print("Base: ", BASE_DIR)
+    print("Front: ", FRONTEND_DIR)
+    print("Static:", STATIC_DIR)
+    app.run(debug=True)
